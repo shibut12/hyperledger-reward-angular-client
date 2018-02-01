5bb6843b3c23f67ef4b4298a77ef1842271cc6f2
shibut12 <shibu@hotmail.co.in>
2018-01-31 23:33:54 -0500

    Fixed the issues that broke build

--- src/app/models/rewardxToken.ts
+++ src/app/models/rewardxToken.ts
@@ -1,2 +1,10 @@
 export class RewardxToken {
+    name: string;
+    value: string;
+    symbol: string;
+    marchentId: string;
+    customerId: string[];
+    customerTokenValue: string[];
+    assignedToMerchantFlag: string;
+    assignedToCustomerFlag: string;
 }
--- src/app/tokendt/tokendt.component.ts
+++ src/app/tokendt/tokendt.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { RewardxToken } from '../models/rewardxToken';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-tokendt',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tokendt.component.scss']
 })
 export class TokendtComponent implements OnInit {
+  @Input() tokens: RewardxToken[];
 
+  displayedColumns = ['name', 'value', 'symbol', 'marchentId', 'assignedToMerchantFlag', 'assignedToCustomerFlag'];
+  dataSource : MatTableDataSource<RewardxToken>;
+  
   constructor() { }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
   ngOnInit() {
+    this.dataSource = new MatTableDataSource<RewardxToken>(this.tokens);
   }
 
 }
